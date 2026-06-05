8588fff3f9394c75be921c8954d63a00eecaede4
GoofyGarry420 <radekmijal@gmail.com>
2026-06-05 11:55:45 +0200

    Base from Samson + start

---
+++ astolfi_278334.m
@@ -0,0 +1,31 @@
+clear;
+clc;
+
+% const ----------------
+Rk = 0.075;
+L = 0.3;
+d = 0;
+Mp = 97;
+Mk = 5;
+Mc = Mp + 2*Mk;
+Ixx = (1/2)*Mk*Rk*Rk;
+Izz = (1/12)*Mk*d*d + (1/4)*Mk*Rk*Rk;
+Iz = 6.609;
+Ic = Iz + 2*Izz + 2*Mk*L*L;
+
+Rd = 2;
+wd = pi/6;
+vd = wd*Rd;
+
+% modyfikowalne ---------
+k1      = 1; 
+k2      = 10; 
+Kd      = 10;
+
+xd0     = 5;
+yd0     = -3;
+thetad0 = 0;
+
+%     x  y  theta  phi1  phi2
+q0 = [0; 0;   0;    0;    0];
+
